33a68e64f53af47beecde21ab668bcdae1906ab8
aistmarufap <aist.maruf@gmail.com>
2025-08-19 12:31:56 +0600

    update

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const Routes = require("./routes/route.js");
+const Routes = require("../routes/route.js");
 
 dotenv.config();
 
